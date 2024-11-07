5bba23e44853f5fc0a60f214978d39ce71b145b5
Daniel B <7193551+BasicWombat@users.noreply.github.com>
2024-11-07 21:34:54 +1100

    Update JsonFetch.java

--- JsonFetch.java
+++ JsonFetch.java
@@ -12,11 +12,20 @@ public class JsonFetch {
         this.jsonData = JsonParser.parseString(jsonDataString).getAsJsonObject();
     }
 
+    
+    /** 
+     * @param textArea
+     */
     public void displayData(JTextArea textArea) {
         String formattedData = formatJson(jsonData, 0);
         SwingUtilities.invokeLater(() -> textArea.setText(formattedData));
     }
 
+    
+    /** 
+     * @param key
+     * @return String
+     */
     public String getValueByKey(String key) {
         if (jsonData != null && jsonData.has(key)) {
             return jsonData.get(key).toString();
@@ -24,11 +33,27 @@ public class JsonFetch {
         return null;
     }
 
+    
+    /** 
+     * @return JsonObject
+     */
     public JsonObject getJsonData() {
         return this.jsonData;
     }
     
 
+    /**
+     * This method is used to get a nested value from the JSON object.
+     * 
+     * @param keys
+     *            This is a variable argument parameter. The last element of
+     *            this array is the key of the value to be fetched. All the
+     *            elements before the last are keys of nested JSON objects.
+     * @return The value of the key which is passed as the last element of the
+     *         array. If any of the keys in the array don't exist in the JSON
+     *         object, or if the JSON object is null, then this method will
+     *         return null.
+     */
     public String getNestedValueByKey(String... keys) {
         JsonObject temp = jsonData;
         for (int i = 0; i < keys.length - 1; i++) {
@@ -41,6 +66,13 @@ public class JsonFetch {
         return temp != null && temp.has(keys[keys.length - 1]) ? temp.get(keys[keys.length - 1]).toString() : null;
     }
 
+
+    /**
+     * Recursively formats a JsonElement into a string with proper indentation.
+     * @param element The JsonElement to format
+     * @param indent The amount of indentation to add to the formatted string
+     * @return The formatted string
+     */
     String formatJson(JsonElement element, int indent) {
         StringBuilder sb = new StringBuilder();
         String indentStr = " ".repeat(indent);
